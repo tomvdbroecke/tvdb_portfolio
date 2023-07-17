b2bbed7d73ff49e71c8078feeb252fdade424ce9
Tom van den Broecke <tvdbroecke98@gmail.com>
2023-07-17 18:24:11 +0200

    Added metatags and favicons

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,8 +23,17 @@ export default function Root({ children }) {
         <html lang="en">
             <head>
                 <title>Tom van den Broecke</title>
-                <meta name="description" content="A portfolio website for Tom van den Broecke."/>
-                <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+                <meta name="author" content="Tom van den Broecke"/>
+                <meta name="description" content="I'm Tom van den Broecke, a software developer based in the Netherlands. My expertise lies in full stack web development, where I thrive in creating seamless user experiences and robust server infrastructures."/>
+                <meta name="keywords" content="Software Engineer, Sofware Developer, Full Stack, NextJs, React, Express, Programming"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000613"/>
+                <meta name="msapplication-TileColor" content="#000613"/>
+                <meta name="theme-color" content="#000613"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>
             </head>
             <body className={`overflow-hidden ${inter.className}`}>
                 <Galaxy {...{ pathname }} />
